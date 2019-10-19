2ec888a1d24ce5fd531c4d5fee504763d1523d2b
Dioceus <chuwei.chen2001@gmail.com>
2019-10-19 14:47:03 -0400

    Authentication Backend added

---
+++ Server-Side/Database/models/Customer.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CustomerSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = Customer = mongoose.model("customers", CustomerSchema);
